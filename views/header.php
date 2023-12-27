7429ec9f03faa01033a88394b4c4f95dc0bae4a8
oussama <freegiftcardishere@gmail.com>
2023-12-27 10:26:57 +0100

    creating the diagram of class

---
+++ views/header.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse d-flex justify-content-between mx-4" id="navbarTogglerDemo01">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+     
+    </ul>
+    <div class="d-flex justify-content-between">
+    <form class="form-inline my-2 my-lg-0" action="index.php" method="POST">
+      <button class="btn btn-success me-2 my-sm-0" type="submit" value="login">register</button>
+    </form>
+    <form class="form-inline my-2 my-lg-0" action="index.php" method="POST">
+      <button class="btn btn-primary my-2 my-sm-0" type="submit" value="login">Login</button>
+    </form>
+    </div>
+  </div>
+</nav>
