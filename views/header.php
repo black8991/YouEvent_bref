22a0af5aa9f44de46b2f843502149c49e541d674
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-01-03 09:57:04 +0100

    front-end admin

--- views/header.php
+++ views/header.php
@@ -1,20 +1,20 @@
-<nav class="navbar navbar-expand-lg bg-dark navbar-light bg-light">
+<nav class="navbar navbar-expand-lg bg-dark navbar-light mb-3">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse d-flex justify-content-between mx-4" id="navbarTogglerDemo01">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-    <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
+    <li class="nav-item active ">
+            <a class="nav-link text-light" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link text-light" href="#">Features</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link text-light" href="#">Pricing</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link text-light" href="#">About</a>
           </li>
     </ul>
 
